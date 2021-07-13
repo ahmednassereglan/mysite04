f09dde9f75ff4ecc428818ea9b317890c52939cb
ahmednassereglan <ahmednassereglan@gmail.com>
2021-07-13 08:45:31 +0200

    finish home section and begin in about section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+    // show navbar when click on icon
     $('i.icon').click(function() {
         $('.nav-list').slideToggle();
 
-    })
+    });
+    // when scroll body sticky navbar
+    $(window).scroll(function() {
+        var sc = $(this).scrollTop();
+        if (sc > 100) {
+            $('header').addClass('sticky');
+        } else {
+            $('header').removeClass('sticky');
+        }
+    });
 });
