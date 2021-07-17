d7a16bfedb9d4ea112f7680b5c03342380665812
ahmednassereglan <ahmednassereglan@gmail.com>
2021-07-17 12:29:14 +0200

    adding owl carousel libraby and finishing Team Section

--- js/script.js
+++ js/script.js
@@ -42,5 +42,24 @@ $(document).ready(function() {
 
     });
 
+    // Owl carousal  >> Team Section
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
+
 
 });
